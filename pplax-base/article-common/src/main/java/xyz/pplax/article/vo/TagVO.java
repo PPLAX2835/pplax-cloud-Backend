fd346725d5b637dc5969e25c47ecd321ade7dbcc
PPLAX2835 <lax1458667357@gmail.com>
2023-08-31 09:21:28 +0800

    pplax-base:     增加pplax-core     增加amqp-common (rabbitmq相关)     增加file-common (文件相关)     增加data-common (查询相关，分页、查询条件等)     增加auth-common (认证相关)     增加article-common  (文章相关)

---
+++ pplax-base/article-common/src/main/java/xyz/pplax/article/vo/TagVO.java
@@ -0,0 +1,19 @@
+package xyz.pplax.article.vo;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import xyz.pplax.article.po.Tag;
+
+/**
+ * @description TODO <br/>
+ * @date 2022-12-14 20:46:02 <br/>
+ * @author xcye <br/>
+ */
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Schema(title = "tag数据表的VO")
+public class TagVO extends Tag {
+
+}
---
+++ pplax-base/article-common/src/main/java/xyz/pplax/article/vo/TalkVO.java
@@ -0,0 +1,19 @@
+package xyz.pplax.article.vo;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import xyz.pplax.article.po.Talk;
+
+/**
+ * @description TODO <br/>
+ * @date 2022-12-14 20:46:02 <br/>
+ * @author xcye <br/>
+ */
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Schema(title = "talk数据表的VO")
+public class TalkVO extends Talk {
+
+}
