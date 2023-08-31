f91c68111059352160e4cf6d2458cb3e6b7a0e14
PPLAX2835 <lax1458667357@gmail.com>
2023-08-31 09:01:11 +0800

    pplax-base:     增加pplax-core     增加amqp-common (rabbitmq相关)     增加file-common (文件相关)

---
+++ pplax-base/file-common/src/main/java/xyz/pplax/file/vo/QiniuReturnBody.java
@@ -0,0 +1,23 @@
+package xyz.pplax.file.vo;
+
+import lombok.Data;
+
+/**
+ * @author xcye
+ * @description
+ * @date 2023-05-07 20:12:42
+ */
+
+@Data
+public class QiniuReturnBody {
+
+    private String bucket;
+
+    private String key;
+
+    private Long fsize;
+
+    private String mimeType;
+
+    private String hash;
+}
