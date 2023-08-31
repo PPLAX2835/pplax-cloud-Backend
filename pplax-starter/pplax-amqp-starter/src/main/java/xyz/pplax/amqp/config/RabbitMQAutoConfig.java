5d9d127d78d32269e24e7881a942196eca9b4730
PPLAX2835 <lax1458667357@gmail.com>
2023-08-31 15:20:24 +0800

    pplax-starter     pplax-spring-boot-starter     pplax-amqp-starter

---
+++ pplax-starter/pplax-amqp-starter/src/main/java/xyz/pplax/amqp/config/RabbitMQAutoConfig.java
@@ -0,0 +1,14 @@
+package xyz.pplax.amqp.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author qsyyke
+ * @date Created in 2022/5/2 17:50
+ */
+
+@EnableConfigurationProperties({PPLAXAmqpProperties.class})
+@Configuration
+public class RabbitMQAutoConfig {
+}
