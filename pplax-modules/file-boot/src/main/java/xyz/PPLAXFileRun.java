d6669f6bf8b6c3214fcd95b4918f12f25df8dd3f
PPLAX2835 <lax1458667357@gmail.com>
2023-09-02 15:15:50 +0800

    file-boot

---
+++ pplax-modules/file-boot/src/main/java/xyz/PPLAXFileRun.java
@@ -0,0 +1,16 @@
+package xyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qsyyke
+ * @date Created in 2022/5/2 15:35
+ */
+
+@SpringBootApplication
+public class PPLAXFileRun {
+    public static void main(String[] args) {
+        SpringApplication.run(PPLAXFileRun.class, args);
+    }
+}
