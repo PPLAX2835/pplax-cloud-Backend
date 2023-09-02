247295968e1aee85a647bc1ee7fec01e91bbcdbd
PPLAX2835 <lax1458667357@gmail.com>
2023-09-02 15:05:57 +0800

    comment-boot

---
+++ pplax-modules/comment-boot/src/main/java/xyz/pplax/comment/config/CommentWebConfig.java
@@ -0,0 +1,46 @@
+package xyz.pplax.comment.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import xyz.pplax.starter.interceptor.PPLAXGlobalHandlerInterceptor;
+
+/**
+ * web配置
+ */
+
+@Configuration
+public class CommentWebConfig implements WebMvcConfigurer {
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /*@Bean
+    @ConditionalOnMissingBean
+    public HttpMessageConverters messageConverters(ObjectProvider<HttpMessageConverter<?>> converters) {
+        return new HttpMessageConverters(converters.orderedStream().collect(Collectors.toList()));
+    }*/
+
+    @Autowired
+    private PPLAXGlobalHandlerInterceptor globalHandlerInterceptor;
+
+    /**
+     * 增加自定义拦截器
+     * @param registry
+     */
+    @Override
+    public void addInterceptors (InterceptorRegistry registry) {
+        //"*/css/**","*/js/**","*/images/**","*/fonts/**"
+        registry.addInterceptor(globalHandlerInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login","/","/css/**","/js/**","/images/**","/fonts/**");
+
+    }
+}
